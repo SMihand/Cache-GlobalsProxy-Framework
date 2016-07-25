d793c90936c197cbaff75ad91f61684a74f329c2
SMihand <avmihand@gmail.com>
2016-07-25 21:09:28 +0300

    Add files via upload

---
+++ SpeedTest_Save/SpeedTest_Save.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace EXTREMEAccessTESTS
+{
+    partial class EXTREMEcomparator
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.probeCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.objectCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nativeCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.trueCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.progressObject = new System.Windows.Forms.ToolStripProgressBar();
+            this.progressNative = new System.Windows.Forms.ToolStripProgressBar();
+            this.progressTrue = new System.Windows.Forms.ToolStripProgressBar();
+            this.progressClear = new System.Windows.Forms.ToolStripProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.probeCol,
+            this.objectCol,
+            this.nativeCol,
+            this.trueCol});
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(1103, 242);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // probeCol
+            // 
+            this.probeCol.HeaderText = "probeNum";
+            this.probeCol.Name = "probeCol";
+            // 
+            // objectCol
+            // 
+            this.objectCol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.objectCol.HeaderText = "object(milisec)";
+            this.objectCol.Name = "objectCol";
+            // 
+            // nativeCol
+            // 
+            this.nativeCol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nativeCol.HeaderText = "native(milisec)";
+            this.nativeCol.Name = "nativeCol";
+            // 
+            // trueCol
+            // 
+            this.trueCol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.trueCol.HeaderText = "true(milisec)";
+            this.trueCol.Name = "trueCol";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInfo.Location = new System.Drawing.Point(12, 262);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(937, 20);
+            this.txtInfo.TabIndex = 2;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTest.Location = new System.Drawing.Point(1032, 260);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(83, 23);
+            this.btnTest.TabIndex = 3;
+            this.btnTest.Text = "TestAgain";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.progressObject,
+            this.progressNative,
+            this.progressTrue,
+            this.progressClear});
+            this.statusStrip.Location = new System.Drawing.Point(0, 291);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(1127, 22);
+            this.statusStrip.TabIndex = 4;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // progressObject
+            // 
+            this.progressObject.Name = "progressObject";
+            this.progressObject.Size = new System.Drawing.Size(200, 16);
+            // 
+            // progressNative
+            // 
+            this.progressNative.Name = "progressNative";
+            this.progressNative.Size = new System.Drawing.Size(200, 16);
+            this.progressNative.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // progressTrue
+            // 
+            this.progressTrue.Name = "progressTrue";
+            this.progressTrue.Size = new System.Drawing.Size(200, 16);
+            // 
+            // progressClear
+            // 
+            this.progressClear.Name = "progressClear";
+            this.progressClear.Size = new System.Drawing.Size(120, 16);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(955, 262);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(71, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            70000,
+            0,
+            0,
+            0});
+            // 
+            // EXTREMEcomparator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 313);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "EXTREMEcomparator";
+            this.Text = "Compararor extreme";
+            this.Load += new System.EventHandler(this.EXTREMEcomparator_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar progressNative;
+        private System.Windows.Forms.ToolStripProgressBar progressTrue;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ToolStripProgressBar progressClear;
+        private System.Windows.Forms.ToolStripProgressBar progressObject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn probeCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn objectCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nativeCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn trueCol;
+    }
+}
+
---
+++ SpeedTest_Save/SpeedTest_Save.cs
@@ -0,0 +1,311 @@
+﻿using InterSystems.Globals;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections;
+
+using CacheEXTREME2.WMetaGlobal;
+using CacheEXTREME2.WDirectGlobal;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Threading;
+using CacheEXTREME2.WProxyGlobal;
+using EXTREMEAccessTESTS.Generated;
+using System.Text;
+
+namespace EXTREMEAccessTESTS
+{
+    public partial class EXTREMEcomparator : Form
+    {
+        private string _namespace = "USER";
+        private string _user = "mihand";
+        private string _password = "19735";
+        public Connection conn;
+        WowsTestContext trueWowsTestContext;
+        //
+        public EXTREMEcomparator()
+        {
+            InitializeComponent();
+        }
+        //
+        private void EXTREMEcomparator_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn == null)
+                {
+                    conn = ConnectionContext.GetConnection();
+                    conn.Connect(_namespace, _user, _password);
+                }
+                trueWowsTestContext = new WowsTestContext(conn);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //
+        private void compareSaveSpeed(int count)
+        {
+            native_VS_true_VS_object(count);
+            GC.Collect();
+            //testObjectSave(count);
+        }
+        private void native_VS_true_VS_object(int count,bool invoke = true)
+        {
+            txtInfo.Text = ""; txtInfo.Refresh();
+            dataGridView.Enabled = false;
+            progressNative.Value = 0;
+            progressTrue.Value = 0;
+            progressObject.Value = 0;
+            killOld(new KillParam(count));
+            progressClear.Value = 0;
+
+            //object
+            List<TimeSpan> objSpans = testObjectSave(count);
+            long Osumm = objSpans.Sum(e => e.Ticks);
+            double Oavrg = objSpans.Average(e => e.TotalMilliseconds);
+            TimeSpan oTotal = new TimeSpan(Osumm);
+            txtInfo.Text += " _ Object: " + oTotal.ToString() + "sec (" + Oavrg + "msec) _"; txtInfo.Refresh();
+            Thread.Sleep(100);
+            killOld(new KillParam(count));
+
+            //native
+            List<TimeSpan> nativeSpans = testNativeSave(count);
+            long Nsumm = nativeSpans.Sum(e => e.Ticks);
+            double Navrg = nativeSpans.Average(e => e.TotalMilliseconds);
+            TimeSpan nTotal = new TimeSpan(Nsumm);
+            txtInfo.Text += " _ Native: " + nTotal.ToString() + "sec (" + Navrg + "msec) _ "; txtInfo.Refresh();
+            Thread.Sleep(100);
+            killOld(new KillParam(count));
+            Thread.Sleep(100);
+
+            //true
+            /*List<TimeSpan> trueSpans = testTrueSave(count);
+            long Tsumm = trueSpans.Sum(e => e.Ticks);
+            double Tmax = trueSpans.Max(e => e.TotalMilliseconds);
+            TimeSpan tTotal = new TimeSpan(Tsumm);
+            txtInfo.Text += " _ True: " + tTotal.ToString() + "(" + Tmax + ") _ "; txtInfo.Refresh();
+            Thread.Sleep(100);
+            killOld(new KillParam(count));
+            Thread.Sleep(100);*/
+
+            //
+            dataGridView.Rows.Clear();
+            int div = 100;
+            if (count >= 10000) { div = 100; }
+            if (count >= 50000) { div = 50; }
+            if (count >= 100000) { div = 100; }
+            if (count >= 200000) {div = 200;}
+            int step = count / div;
+
+            /*for (int i = 0, j = 0; i < count; i += step, j++)
+            {
+                dataGridView.Rows.Add();
+                dataGridView["probeCol", j].Value = i;
+                dataGridView["nativeCol", j].Value = nativeSpans[0].TotalMilliseconds;
+                //dataGridView["trueCol", j].Value = trueSpans[0].TotalMilliseconds;
+                dataGridView["objectCol", j].Value = objSpans[0].TotalMilliseconds;
+                objSpans.RemoveAt(0);
+                //trueSpans.RemoveAt(0);
+                nativeSpans.RemoveAt(0);
+            }*/
+            txtInfo.Text += " _ obj/nat= " + ((double)Osumm / (double)Nsumm).ToString(); txtInfo.Refresh();
+            dataGridView.Enabled = true;
+        }
+
+        private List<TimeSpan> testNativeSave(int count = 100)
+        {
+            List<TimeSpan> spans = new List<TimeSpan>();
+            NodeReference wtr = conn.CreateNodeReference("wowsTest");
+            Stopwatch nWatch = new Stopwatch();
+            progressNative.Value = 0;
+            //
+            //Dataholders
+            string shipClass = "kruis";
+            object[] keys = new object[] { shipClass, 0 };
+            object[] oficersNames = new object[] { "", "", "" };
+            object[] workersNames = new object[] { "", "", "" };
+            StringBuilder name1 = new StringBuilder(20);
+            StringBuilder name2 = new StringBuilder(20);
+            StringBuilder name3 = new StringBuilder(20);
+            int s = 1000;
+            byte[] byteValue = new byte[] { 6, 6, 6 };
+            //
+            for (int i = 0; i < count; i++)
+            {
+                name1.Remove(0, name1.Length);
+                name2.Remove(0, name2.Length);
+                name3.Remove(0, name3.Length);
+                name1.Insert(0, i + 1);
+                name2.Insert(0, i + 2);
+                name3.Insert(0, i + 3);
+
+                //отсюда
+                keys[0] = shipClass;
+                keys[1] = i;
+                oficersNames[0] = name1.ToString();
+                oficersNames[1] = name2.ToString();
+                oficersNames[2] = name3.ToString();
+                workersNames[0] = name3.ToString();
+                workersNames[1] = name2.ToString();
+                workersNames[2] = name1.ToString();
+                s = s;
+                byteValue = byteValue;
+                //досюда
+                {
+                    nWatch.Reset();
+                    nWatch.Start();
+
+                    ValueList oficersVL = conn.CreateList();
+                    oficersVL.Append(oficersNames);
+                    ValueList workersVL = conn.CreateList();
+                    workersVL.Append(workersNames);
+                    ValueList vlist = conn.CreateList();
+                    vlist.Append(oficersVL);
+                    vlist.Append(workersVL);
+                    vlist.Append(s);
+                    vlist.Append(byteValue);
+                    wtr.Set(vlist, keys);
+
+                    nWatch.Stop();
+                }
+
+                TimeSpan nTS = nWatch.Elapsed;
+                spans.Add(nTS);
+                progressNative.Value = i * 100 / count;
+            }
+            progressNative.Value = 100;
+            return spans;
+        }
+        private List<TimeSpan> testTrueSave(int count = 100)
+        {
+            List<TimeSpan> spans = new List<TimeSpan>();
+            TrueNodeReference WTr = new TrueNodeReference(conn, "wowsTest");
+            Stopwatch tWatch = new Stopwatch();
+            progressTrue.Value = 0;
+            for (int i = 0; i < count; i++)
+            {
+                string i1 = (i + 1).ToString(), i2 = (i + 2).ToString(), i3 = (i + 3).ToString();
+                int s = 1000;
+                //ОТСЮДА
+                ArrayList akeys = new ArrayList() { "kruis", i };
+                ArrayList asublist1 = new ArrayList() { i1, i2, i3 };
+                ArrayList asublist2 = new ArrayList() { i3, i2, i1 };
+                byte[] bytes = new byte[] { 6, 6, 6 };
+                ArrayList alist = new ArrayList() { asublist1, asublist2, s, bytes };
+                ArrayList values = new ArrayList() { 
+                    new ArrayList() { i1, i2, i3 }
+                    , new ArrayList() { i3, i2, i1 }
+                    , 1000
+                    , new byte[] { 6, 6, 6 } };
+                //Досюда
+                {
+                    tWatch.Reset();
+                    tWatch.Start();
+                    
+                    WTr.SetValues(akeys, alist);
+                    
+                    tWatch.Stop();
+                }
+                TimeSpan tTS = tWatch.Elapsed;
+                spans.Add(tTS);
+                progressTrue.Value = i * 100 / count;
+            }
+            progressTrue.Value = 100;
+            return spans;
+        }
+        private List<TimeSpan> testObjectSave(int count = 100)
+        {
+            List<TimeSpan> spans = new List<TimeSpan>();
+            TrueNodeReference WTr = new TrueNodeReference(conn, "wowsTest");
+            Stopwatch oWatch = new Stopwatch();
+            progressObject.Value = 0;
+            //
+            trueWowsTestContext.stuffManager.Validate = false;
+            Random rand = new Random();
+            //
+            //Data holders
+            string shipClass = "kruis";
+            int s = 1000;
+            List<string> oficersNames = new List<string> { "", "", "" };
+            List<string> workersNames = new List<string> { "", "", "" };
+            StringBuilder name1 = new StringBuilder(20);
+            StringBuilder name2 = new StringBuilder(20);
+            StringBuilder name3 = new StringBuilder(20);
+            byte[] byteValue = new byte[] { 6, 6, 6 };
+            stuffProxy entity = new stuffProxy();
+            //
+            for (int i = 0; i < count; i++)
+            {
+                name1.Remove(0, name1.Length);
+                name2.Remove(0, name2.Length);
+                name3.Remove(0, name3.Length);
+                name1.Insert(0, i + 1);
+                name2.Insert(0, i + 2);
+                name3.Insert(0, i + 3);
+                oficersNames[0] = name1.ToString();
+                oficersNames[1] = name2.ToString();
+                oficersNames[2] = name3.ToString();
+                workersNames[0] = name3.ToString();
+                workersNames[1] = name2.ToString();
+                workersNames[2] = name1.ToString();
+                //ОТСЮДА
+                entity.shipclass = shipClass;
+                entity.shiprank = i;
+                entity.officers = oficersNames;
+                entity.workers = workersNames;
+                entity.workerSalary = s;
+                entity.photo = byteValue;
+                //Досюда
+                {
+                    oWatch.Reset();
+                    oWatch.Start();
+
+                    trueWowsTestContext.stuffManager.Save(entity);
+
+                    oWatch.Stop();
+                }
+                TimeSpan tTS = oWatch.Elapsed;
+                spans.Add(tTS);
+                progressObject.Value = i * 100 / count;
+            }
+            progressObject.Value = 100;
+            return spans;
+        }
+        //
+        class KillParam
+        {
+            public int param;
+            public KillParam(int param) { this.param = param; }
+        }
+        void killOld(object param)
+        {
+            int count = ((KillParam)param).param;
+            progressClear.Value = 0;
+
+            NodeReference wtr = conn.CreateNodeReference("wowsTest");
+            object[] akeys = new object[2]{"kruis",0};
+            for (int i = 0; i < count; i++)
+            {
+                akeys[1] = i;
+                wtr.Kill(akeys);
+                progressClear.Value = i * 100 / count;
+            }
+            progressClear.Value = 100;
+        }
+        //
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            int count = (int)numericUpDown1.Value;
+            if (MessageBox.Show("At the moment this test may force a lot of RAM, it may temporarily slow your system. For example, saving 300000 units takes approximately 2Gb of RAM.\n\n Do you want to continue?", "WARNING!!!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                compareSaveSpeed(count);
+            }
+            /*Thread tr = new Thread(new ParameterizedThreadStart(killOld));
+            tr.Start(new KillParam(count));*/
+        }
+    }
+}

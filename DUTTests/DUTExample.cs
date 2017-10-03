42c153e386b89d018ff4cb59cecb89b5447805fb
Unknown <avmihand@gmail.com>
2017-10-03 18:11:59 +0300

    Added DUTexample (MAY NOT WORK)
    cause of dark inspiration

---
+++ DUTTests/DUTExample.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntitiesGenerationTests
+{
+    class StatisticProxy
+    {
+        public List<ChangeKey> Changes;
+
+        public string da;
+        public string db;
+        public string dc;
+
+        public StatisticProxy(List<ChangeKey> keys, string da, string db, string dc)
+        {
+            this.Changes = keys;
+            this.da = da;
+            this.db = db;
+            this.dc = dc;
+        }
+
+        public StatisticProxy()
+        {
+            Changes = new List<ChangeKey>();
+            db = "n";
+            da = "n";
+            dc = "n";
+        }
+
+        public bool StatisticProxyValidator(StatisticProxy entity)
+        {
+            return true;
+        }
+    }
+
+    public class ChangeKey
+    {
+        public string a;
+        public string b;
+        public int? d;
+
+        public ChangeKey()
+        {
+            this.a = "n";
+            this.b = "n";
+            this.d = 0;
+        }
+
+        public ChangeKey(string a, string b, int? d)
+        {
+            this.a = a;
+            this.b = b;
+            this.d = d;
+        }
+    }
+
+    public class ChangeSubKey
+    {
+        public double? a;
+        public string b;
+        public int? c;
+
+        public ChangeSubKey()
+        {
+            this.a = 0;
+            this.b = "n";
+            this.c = 0;
+        }
+
+        public ChangeSubKey(double? a, string b, int? c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+}
---
+++ DUTTests/StatManager.cs
@@ -0,0 +1,77 @@
+﻿using CacheEXTREME2.WProxyGlobal;
+using EntitiesGenerationTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DUTTests
+{
+
+    class DUTStatManager<StatKey, StatValue> where StatValue : class where StatKey : class
+    {
+        DUTManager<StatKey, StatValue> statistic;
+
+        int statSequenceMaxCount;
+        Delegate onFullSequenceHandler;
+        List<StatSequence<StatKey>> sequences;
+
+        Queue<StatKey> statQueue;
+
+        //
+        public DUTStatManager(int statSequenceMaxCount, Delegate onFullSequenceHandler)
+        {
+            this.statSequenceMaxCount = statSequenceMaxCount;
+            this.onFullSequenceHandler = onFullSequenceHandler;
+            this.sequences = new List<StatSequence<StatKey>>();
+            this.statQueue = new Queue<StatKey>();
+        }
+        //
+
+        public void AddStat(StatKey stat)
+        {
+            statQueue.Enqueue(stat);
+            //
+            //statQueue must be free on enqueue!!! while stat is adding to sequences
+            //
+            StatSequence<StatKey> newSequence = new StatSequence<StatKey>(statSequenceMaxCount, onFullSequenceHandler);
+            sequences.Add(newSequence);
+            for (int i = 0; i < sequences.Count; i++)
+			{
+                sequences[i].AddStat(stat);
+                if (sequences[i].CurrentCount == statSequenceMaxCount)
+                {
+                    sequences.Remove(sequences[i]);
+                }
+            }
+            //
+            //??? is statQueue must be blocked after adding!!!
+            //
+            statQueue.Dequeue();
+        }
+    }
+
+    class StatSequence<StatKey> where StatKey: class
+    {
+        int sequenceMaxCount;
+        public int CurrentCount { get { return statKeys.Count; } }
+        List<StatKey> statKeys;
+        Delegate onFullCollectionHandler;
+        //
+        public StatSequence(int sequenceMaxCount, Delegate onFullCollectionHandler)
+        {
+            this.statKeys = new List<StatKey>(sequenceMaxCount);
+            this.sequenceMaxCount = sequenceMaxCount;
+            this.onFullCollectionHandler = onFullCollectionHandler;
+        }
+        //
+        public void AddStat(StatKey stat)
+        {
+            statKeys.Add(stat);
+            if (statKeys.Count == sequenceMaxCount)
+            {
+                onFullCollectionHandler.DynamicInvoke(new object[] { statKeys });
+            }
+        }
+    }
+}
